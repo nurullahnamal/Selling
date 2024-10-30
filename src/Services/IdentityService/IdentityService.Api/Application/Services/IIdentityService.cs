82d6eb1fdfc566bbfed1960562ca01f77aea64a7
nurullah <nurullahnamal4@gmail.com>
2024-10-30 19:51:55 +0300

    Identity Service

---
+++ src/Services/IdentityService/IdentityService.Api/Application/Services/IIdentityService.cs
@@ -0,0 +1,11 @@
+﻿using IdentityService.Api.Application.Models;
+using System.Threading.Tasks;
+
+namespace IdentityService.Api.Application.Services
+{
+	public interface IIdentityService
+	{
+		Task<LoginResponseModel> Login(LoginRequestModel requestModel);
+
+	}
+}
